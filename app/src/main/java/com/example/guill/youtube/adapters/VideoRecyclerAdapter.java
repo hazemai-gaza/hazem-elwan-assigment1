5940e259229fce260ed05d87a3549fe358be6fe7
hazemai-gaza <ahmed123456789.poiu@gmail.com>
2025-08-22 20:16:19 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/guill/youtube/adapters/VideoRecyclerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.guill.youtube.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.example.guill.youtube.R;
+import com.example.guill.youtube.viewholders.VideoViewHolder;
+import com.example.guill.youtube.models.Item;
+
+import java.util.List;
+
+public class VideoRecyclerAdapter extends RecyclerView.Adapter<VideoViewHolder>{
+    private final List<Item> videos;
+
+    public VideoRecyclerAdapter(List<Item> videos) {
+        this.videos = videos;
+    }
+
+    public List<Item> getVideos() {
+        return videos;
+    }
+
+    @Override
+    public VideoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new VideoViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_video, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(VideoViewHolder holder, int position) {
+        holder.bind(videos.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return videos != null ? videos.size() : 0;
+    }
+}
